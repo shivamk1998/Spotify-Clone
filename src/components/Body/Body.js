efde0b42bc7f723aad5880fe968143b3db6ea856
shivamk1998 <shivamkumarsingh1998@gmail.com>
2023-08-23 19:05:07 +0530

    updated body.

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -2,6 +2,11 @@ import React from 'react'
 import './Body.css'
 import Header  from '../Header/Header'
 import {useDatalayerValue} from '../../Data/DataLayer'
+import PlayCircleFilledIcon from '@mui/icons-material/PlayCircleFilled'; 
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import SongRow from './SongRow';
+
 function Body({spotify}) {
 
   const [{discover_weekly}, dispatch]= useDatalayerValue()
@@ -15,9 +20,19 @@ function Body({spotify}) {
         <div className="body_infoText">
           <strong>PLAYLIST</strong>
           <h2>Discover Weekly</h2>
-          <p>description...</p>
           <p>{discover_weekly?.description}</p>
+        </div> 
+      </div>
+      <div className="body_songs">
+        <div className="body_icons">
+          <PlayCircleFilledIcon className='body_shuffle'/>
+          <FavoriteIcon fontSize='large'/>
+          <MoreHorizIcon/>
         </div>
+
+        {discover_weekly?.tracks.items?.map((item) => (
+  <SongRow track={item.track} />
+))}
       </div>
 
     </div>
